9fc6590f69136152d7c6a0e160ae64e745f0287c
Zacks <53922473+Zackperez@users.noreply.github.com>
2023-04-03 02:43:08 -0500

    Add files via upload

--- javascript/operaciones-js.js
+++ javascript/operaciones-js.js
@@ -1,10 +1,15 @@
+// Cuando cargue toda la página se ejecutará lo que esté dentro
+
+const tbody = document.getElementById('table-body');
+
+
 document.addEventListener('DOMContentLoaded', function () {
-  const tbody = document.querySelector('table tbody');
+
+  /* Se llena automaticamente la tabla */
 
   fetch('http://127.0.0.1:4000/mostrar_datos_tabla/')
     .then(response => response.json())
     .then(data => {
-      const tbody = document.getElementById('table-body');
       data.forEach(datos => {
         const tr = document.createElement('tr');
         tr.innerHTML = `
@@ -18,71 +23,244 @@ document.addEventListener('DOMContentLoaded', function () {
       });
     });
 })
+// Fin del cargue de documentos
 
-// Get the modal
-var modal = document.getElementById("myModal");
+/* MODAL INSERTAR */
+//Boton para abrir el modal
+var btnModalInsertar = document.getElementById("btnModalInsertar");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+// Target del modal
+var modalInsertar = document.getElementById("targetModalInsertar");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// icono para cerrar el modal 
+var span = document.getElementsByClassName("close-insertar")[0];
+
+//Boton dentro del modal
+const btnInsertarDatosModal = document.getElementById('btnInsertarDatosModal');
 
 // When the user clicks the button, open the modal 
-btn.onclick = function () {
-  modal.style.display = "block";
+btnModalInsertar.onclick = function () {
+  modalInsertar.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function () {
-  modal.style.display = "none";
+  modalInsertar.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == modalInsertar) {
+    modalInsertar.style.display = "none";
   }
 }
 
-const boton = document.getElementById("btn_consultar");
+/* MODAL MODIFICAR */
+//Boton para abrir el modal
+var btnModalModificar = document.getElementById("btnModalModificar");
 
-/*boton.onclick = function () {
-  mostrar_respuesta();
+// Target del modal
+var modalModificar = document.getElementById("targetModalModificar");
+
+// icono para cerrar el modal 
+var span = document.getElementsByClassName("close-modificar")[0];
+
+//Boton dentro del modal
+const btnModificarDatosModal = document.getElementById('btnModificarDatosModal');
+
+// When the user clicks the button, open the modal 
+btnModalModificar.onclick = function () {
+  modalModificar.style.display = "block";
 }
 
-function mostrar_respuesta() {
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modalModificar.style.display = "none";
+}
 
-}*/
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modalModificar) {
+    modalModificar.style.display = "none";
+  }
+}
 
-const boton_modal = document.getElementById('boton-modal');
+/* MODAL Eliminar */
+//Boton para abrir el modal
+var btnModalEliminar = document.getElementById("btnModalEliminar");
 
-function mostrar_valores() {
-  const campo_modelo = document.getElementById("modelo").value;
-  const campo_año = document.getElementById("año").value;
+// Target del modal
+var modalEliminar = document.getElementById("targetModalEliminar");
+
+// icono para cerrar el modal 
+var span = document.getElementsByClassName("close-eliminar")[0];
+
+//Boton dentro del modal
+const btnEliminarDatosModal = document.getElementById('btnEliminarDatosModal');
+
+// When the user clicks the button, open the modal 
+btnModalEliminar.onclick = function () {
+  modalEliminar.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modalEliminar.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modalEliminar) {
+    modalEliminar.style.display = "none";
+  }
+}
+
+/* MODAL Consultar */
+//Boton para abrir el modal
+var btnModalConsultar = document.getElementById("btnModalConsultar");
+
+// Target del modal
+var modalConsultar = document.getElementById("targetModalConsultar");
+
+// icono para cerrar el modal 
+var span = document.getElementsByClassName("close-consultar")[0];
+
+//Boton dentro del modal
+const btnConsultarDatosModal = document.getElementById('btnConsultarDatosModal');
+
+// When the user clicks the button, open the modal 
+btnModalConsultar.onclick = function () {
+  modalConsultar.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modalConsultar.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modalConsultar) {
+    modalConsultar.style.display = "none";
+  }
+}
+
+
+
+function modificar_datos() {
+  //"UPDATE proovedores SET Nombre =?, Celular = ?, Direccion = ?, Correo = ? WHERE idProveedor = ?";
+  const modificarId = document.getElementById('modificar-id').value;
+  const modificarModelo = document.getElementById('campo-modificar-modelo__combobox').value;
+  const modificarAño = document.getElementById('campo-modificar-year__combobox').value;
+  const modificarPrecio = document.getElementById('modificar-precio').value;
+  const modificarKilometraje = document.getElementById('modificar-kilometraje').value;
+  let formato_precio = Intl.NumberFormat("de-DE");
+  let formato_kilometraje = Intl.NumberFormat("de-DE");
+
+  if (modificarId == "" || modificarModelo == "" || modificarAño == "" || modificarPrecio == "" || modificarKilometraje == "") {
+    alert("Asegurate que los campos no estén vacíos");
+  } else {
+
+    precio = formato_precio.format(modificarPrecio);
+    kilometraje = formato_kilometraje.format(modificarKilometraje);
+
+    const respuestas_preguntas = {
+      modificarId: modificarId,
+      modificarModelo: modificarModelo,
+      modificarAño: modificarAño,
+      modificarKilometraje: kilometraje + " km",
+      modificarPrecio: "$ " + precio
+    }
+
+    axios({
+      method: "POST",
+      url: "http://127.0.0.1:4000/modificar_vehiculo",
+      data: respuestas_preguntas,
+  })
+      .then(res =>
+          console.log(res))
+      .catch(err => console.log('Error:', err))
+
+      modificarId.value = "";
+      modificarModelo.value = "";
+      modificarAño.value = "";
+      modificarPrecio.value = "";
+      modificarKilometraje.value = "";
+  }
+
+}
+
+function eliminar_datos() {
+  const eliminarId = document.getElementById('eliminar-id').value;
+
+
+  if (eliminarId == "") {
+    alert("Asegurate que los campos no estén vacíos");
+  } else {
+
+    const respuestas_preguntas = {
+      eliminarId: eliminarId
+    }
+
+    axios({
+      method: "DELETE",
+      url: "http://127.0.0.1:4000/eliminar_vehiculo",
+      data: respuestas_preguntas,
+  })
+      .then(res =>
+          console.log(res))
+      .catch(err => console.log('Error:', err))
+  }
+
+}
+
+/* Botones */
+btnModificarDatosModal.onclick = function () {
+  modificar_datos();
+}
+
+btnEliminarDatosModal.onclick = function () {
+  eliminar_datos();
+}
+
+
+
+/* Funciones */
+
+function insertar_valores() {
+  const campo_modelo = document.getElementById('campo-modelo__combobox').value;
+  const campo_año = document.getElementById('campo-year__combobox').value;
   const campo_kilometraje = document.getElementById("kilometraje").value;
   const campo_precio = document.getElementById("precio").value;
+  let formato_precio = Intl.NumberFormat("de-DE");
+  let formato_kilometraje = Intl.NumberFormat("de-DE");
 
-  precio = new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 3 }).format(campo_precio);
-  
-  const datos_insertar = {
-    campo_modelo: campo_modelo,
-    campo_año: campo_año,
-    campo_kilometraje: campo_kilometraje+" km",
-    campo_precio: "$ "+precio
+
+  if (campo_kilometraje == "" || campo_precio == "") {
+    alert("Asegurate que los campos no estén vacíos");
+  } else {
+    precio = formato_precio.format(campo_precio);
+    kilometraje = formato_kilometraje.format(campo_kilometraje);
+
+
+    const datos_insertar = {
+      campo_modelo: campo_modelo,
+      campo_año: campo_año,
+      campo_kilometraje: kilometraje + " km",
+      campo_precio: "$ " + precio
+    }
+
+    axios({
+      method: "POST",
+      url: "http://127.0.0.1:4000/insertar_datos/",
+      data: datos_insertar
+    })
+      .then(res =>
+        console.log(res))
+      .catch(err => console.log('Error:', err))
   }
-
-  axios({
-    method: "POST",
-    url: "http://127.0.0.1:4000/insertar_datos/",
-    data: datos_insertar
-  })
-    .then(res =>
-      console.log(res))
-    .catch(err => console.log('Error:', err))
-
 }
 
-boton_modal.onclick = function () {
-  mostrar_valores()
-}
+btnEliminarDatosModal
+
+
